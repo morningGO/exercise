eabddb7ce36d7f0a0cc6229f0a43eb8eb2950342
UmedaTakefumi <ut+github@azurestone.net>
2014-08-10 04:07:12 +0900

    Add:  2014年07月31日の宿題の回答を追加

---
+++ 20140731/UmedaTakefumi/normal/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var value1 string = "ABC"
+  var value2 string = "HOGE"
+  var value3 string = ""
+
+  value3 = value1 + value2
+
+  fmt.Println(value3)
+
+
+}
